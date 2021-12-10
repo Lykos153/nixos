b333ffef215139c64e2e4c08cb3a6dc3cb5e76b2
Silvio Ankermann <silvio@booq.org>
2021-12-10 23:01:45 +0100

    Add podman and x11docker

--- system/configuration.nix
+++ system/configuration.nix
@@ -103,6 +103,7 @@
   '';
   programs.zsh.promptInit = ""; # otherwise it'll override the grml prompt
 
+  virtualisation.podman.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
