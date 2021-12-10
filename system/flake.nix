5d72e074432abb9ccc7e288a06ba00c13e8378db
Silvio Ankermann <silvio@booq.org>
2021-12-10 21:08:58 +0100

    Add url to comment

--- system/flake.nix
+++ system/flake.nix
@@ -3,7 +3,7 @@
   outputs = { self, nixpkgs }:
     {
         nixosConfigurations = {
-            # TODO: Learn how to do that more elegantly 
+            # TODO: Learn how to do that more elegantly, eg. https://www.reddit.com/r/NixOS/comments/jktqpg/choosing_imported_configurations_based_on_a/
             pc = nixpkgs.lib.nixosSystem {
                 system = "x86_64-linux";
                 modules = [ ./configuration.nix ./hardware.d/pc.nix ];
