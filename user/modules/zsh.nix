f706aca3cdc53bd34282967279a7277d284f9d41
Silvio Ankermann <silvio@booq.org>
2021-12-13 14:26:01 +0100

    Fix indentation

--- user/modules/zsh.nix
+++ user/modules/zsh.nix
@@ -46,24 +46,24 @@
       bindkey "^X^E" edit-command-line
 
       gesehen () {
-	CDIR=$(pwd)
-	DIR=$(dirname $1)
-	FILE=$(basename $1)
-	shift
-	cd $DIR
-	git annex metadata --tag gesehen -s gesehen+=`date '+%F@%H-%M'` $FILE $@
-	cd $CDIR
+        CDIR=$(pwd)
+        DIR=$(dirname $1)
+        FILE=$(basename $1)
+        shift
+        cd $DIR
+        git annex metadata --tag gesehen -s gesehen+=`date '+%F@%H-%M'` $FILE $@
+        cd $CDIR
       }
 
       film () {
-	#ambi on
-	vlc "$@"
-	echo "Gesehen? "
-	read yn
-	case $yn in
-		[YyJj]* ) gesehen "$@";;
-	esac
-	#ambi off
+        #ambi on
+        vlc "$@"
+        echo "Gesehen? "
+        read yn
+        case $yn in
+          [YyJj]* ) gesehen "$@";;
+        esac
+        #ambi off
       }
     '';
 
