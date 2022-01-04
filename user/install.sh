dd07de46fe630da67460830c3cd569ccce22e7b0
Silvio Ankermann <silvio@booq.org>
2022-01-05 00:47:32 +0100

    Fix install script

--- user/install.sh
+++ user/install.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 # Inspired by https://github.com/mjlbach/nix-dotfiles/blob/master/home-manager/install.sh
-nix-shell -p nixUnstable --command "nix build --experimental-features 'nix-command flakes' '.#homeConfigurations.nixos.activationPackage'"
+#nix-shell -p nixUnstable --command "nix build --experimental-features 'nix-command flakes' '.#homeConfigurations.nixos.activationPackage'"
+SCRIPTPATH=$(dirname $(readlink -f $0))
+nix build "$SCRIPTPATH"'#homeConfigurations.home.activationPackage'
+result/activate
+rm result
