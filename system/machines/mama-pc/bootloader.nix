979010a728eacd3a7a5dfc717152bc4bce7fc1e6
Silvio Ankermann <silvio@booq.org>
2022-01-07 12:12:51 +0100

    Default to win10 and enable efi on mama-pc

--- system/machines/mama-pc/bootloader.nix
+++ system/machines/mama-pc/bootloader.nix
@@ -1,3 +1,14 @@
 {
-  boot.loader.grub.device = "/dev/disk/by-id/ata-WDC_WD3200AAKS-00UU3A0_WD-WCAYU3038785";
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = false;
+    };
+    grub = {
+      device = "/dev/disk/by-id/ata-WDC_WD3200AAKS-00UU3A0_WD-WCAYU3038785";
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+      useOSProber = true;
+      default = "2";
+    };
+  };
 }
