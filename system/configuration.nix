0d0ec77e4c64a4da979883ce8438425c014a489f
Silvio Ankermann <silvio@booq.org>
2022-01-11 13:42:15 +0100

    Enable systemd-resolved

--- system/configuration.nix
+++ system/configuration.nix
@@ -24,6 +24,11 @@
 
   networking.networkmanager.enable = true;
 
+  services.resolved = {
+    enable = true;
+    fallbackDns = [ "1.1.1.1" "2606:4700:4700::1111,2606:4700:4700::1001" ];
+  };
+
   time.timeZone = "Europe/Berlin";
 
   networking.useDHCP = false;
