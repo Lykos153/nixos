25d45515bb94d42e424d19f7a3d0123079fb3167
Silvio Ankermann <silvio@booq.org>
2022-03-10 10:41:03 +0100

    Add deps for diplomarbeit to silvio-pc

--- system/machines/silvio-pc/itsec-deps.nix
+++ system/machines/silvio-pc/itsec-deps.nix
@@ -5,6 +5,11 @@
 
   virtualisation.libvirtd.enable = true;
   # virtualisation.virtualbox.host.enable = true;
+  
+  services.nfs.server.enable = true;
+  networking.firewall.extraCommands = ''
+    ip46tables -I INPUT 1 -i vboxnet+ -p tcp -m tcp --dport 2049 -j ACCEPT
+  '';
 
   programs.wireshark.enable = true;
 }
