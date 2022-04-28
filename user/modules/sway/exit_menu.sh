6c96de43f734d000696226a30cf3b741092e24d2
Silvio Ankermann <silvio@booq.org>
2022-04-28 08:34:45 +0200

    user sway: clean exit

--- user/modules/sway/exit_menu.sh
+++ user/modules/sway/exit_menu.sh
@@ -4,5 +4,5 @@ while [ "$select" != "NO" -a "$select" != "YES" ]; do
     [ -z "$select" ] && exit 0
 done
 [ "$select" = "NO" ] && exit 0
-systemctl --user stop sway-session.target
+systemctl --user stop sway-session.target &&
 swaymsg exit
