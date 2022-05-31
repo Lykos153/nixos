d54c666ca0d3974083c0c5552825232cfb27ffb0
Silvio Ankermann <silvio@booq.org>
2022-05-31 16:09:26 +0200

    system: fix grub for stick2

--- system/machines/stick2/bootloader.nix
+++ system/machines/stick2/bootloader.nix
@@ -1,3 +1,12 @@
 {
-  boot.loader.grub.device = "/dev/disk/by-id/usb-SanDisk_Extreme_Pro_55345678C7CE-0";
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = false;
+    };
+    grub = {
+      device = "nodev";
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+    };
+  };
 }
