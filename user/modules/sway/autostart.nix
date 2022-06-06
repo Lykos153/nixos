2c5f849943a29faebae40562273c3bf9900e2a6a
Silvio Ankermann <leila@booq.org>
2022-06-06 16:41:57 +0200

    Remove nextcloud-client

--- user/modules/sway/autostart.nix
+++ user/modules/sway/autostart.nix
@@ -4,10 +4,6 @@ let
     echo | ${pkgs.libsecret}/bin/secret-tool store --label=dummy dummy dummy &&
     exec ${pkgs.element-desktop}/bin/element-desktop
   '';
-  nextcloud-client = pkgs.writeShellScriptBin "nextcloud" ''
-    echo | ${pkgs.libsecret}/bin/secret-tool store --label=dummy dummy dummy &&
-    exec ${pkgs.nextcloud-client}/bin/nextcloud $@
-  '';
 in
 {
   systemd.user.services.firefox = {
@@ -65,22 +61,6 @@ in
         ${element-desktop}/bin/element-desktop
       '';
       Restart = "on-failure";
-    };
-  };
-
-  systemd.user.services.nextcloud-client = {
-    Unit = {
-      Description = "Nextcloud Client";
-      PartOf = [ "sway-session.target" ];
-      Wants = [ "keepassxc.service" ];
-      After = [ "keepassxc.service" ];
-    };
-
-    Install.WantedBy = [ "sway-session.target" ];
-
-    Service = {
-      Environment = "PATH=${config.home.profileDirectory}/bin";
-      ExecStart = "${nextcloud-client}/bin/nextcloud --background";
     };
   };
 
