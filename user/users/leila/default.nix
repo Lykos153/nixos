3b9635f3fff84e4928a23fc6c1e6425fc030df6f
Silvio Ankermann <leila@booq.org>
2022-08-17 16:33:33 +0200

    user leila: migrate most packages from arch

--- user/users/leila/default.nix
+++ user/users/leila/default.nix
@@ -1,6 +1,28 @@
+{pkgs, ...}:
 {
   programs.git.userEmail = "leila@booq.org";
   programs.git.userName = "Leila Höll";
+
+  home.packages = with pkgs; [
+    xboxdrv
+
+    android-file-transfer
+
+    gnome.gedit
+    feh # image viewer
+    darktable
+    solaar # TODO udev rules https://search.nixos.org/packages?channel=unstable&show=solaar&from=0&size=50&sort=relevance&type=packages&query=solaar
+
+    tdesktop # telegram
+
+    # unfree
+    zoom-us
+    spotify
+    steam
+    geogebra
+    gitkraken
+    playonlinux
+  ];
 
   imports = [
     ./sway.nix
