d1646ddc901a5096cceb0a52676a58bf72759d7b
Silvio Ankermann <leila@booq.org>
2022-08-19 17:13:05 +0200

    leila: autostart telegram

--- user/modules/sway/autostart.nix
+++ user/modules/sway/autostart.nix
@@ -54,4 +54,19 @@
     };
   };
 
+  systemd.user.services.telegram-desktop = {
+    Unit = {
+      PartOf = [ "sway-session.target" ];
+    };
+
+    Install.WantedBy = [ "sway-session.target" ];
+
+    Service = {
+      ExecStart = ''
+        ${pkgs.tdesktop}/bin/telegram-desktop
+      '';
+      Restart = "on-failure";
+    };
+  };
+
 }
