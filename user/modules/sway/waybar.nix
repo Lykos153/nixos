ef70a8c5abee9de056c1d520df5a093e6ea469dc
Silvio Ankermann <leila@booq.org>
2022-08-26 22:48:16 +0200

    leila waybar: enable gammastep indicator

--- user/modules/sway/waybar.nix
+++ user/modules/sway/waybar.nix
@@ -42,9 +42,9 @@ in
       "sway/window"
     ];
     modules-right = [
-      # "custom/redshift"
       "idle_inhibitor"
       "pulseaudio"
+      "custom/redshift"
       "tray"
       "clock#date"
       "clock#time"
