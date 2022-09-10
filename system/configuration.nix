0fd8766e9e6feefa32069abc502020eccc828580
Silvio Ankermann <silvio@booq.org>
2022-09-10 22:48:15 +0200

    system: disable avahi
    was causing hangups on silvio-pc

--- system/configuration.nix
+++ system/configuration.nix
@@ -51,7 +51,7 @@
 
   # https://nixos.wiki/wiki/Printing
   services.printing.enable = true;
-  services.avahi.enable = true;
+  #services.avahi.enable = true; # <- resulted in hangups on silvio-pc
   # Important to resolve .local domains of printers, otherwise you get an error
   # like  "Impossible to connect to XXX.local: Name or service not known"
   services.avahi.nssmdns = true;
