da8b429c5f297ca56b92d2af1d6404dac45108c2
Silvio Ankermann <silvio@booq.org>
2022-09-13 13:43:47 +0200

    system: explicitly enable firewall

--- system/configuration.nix
+++ system/configuration.nix
@@ -192,11 +192,10 @@
   # https://unix.stackexchange.com/questions/344402/how-to-unlock-gnome-keyring-automatically-in-nixos
   services.gnome.gnome-keyring.enable = true;
 
+  networking.firewall.enable = true;
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
