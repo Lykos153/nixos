2cce0337217ab16452a04dc148f16b5eced62e2b
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2022-11-10 12:39:36 +0100

    system printer: add gutenprint drivers

--- system/configuration.nix
+++ system/configuration.nix
@@ -51,6 +51,7 @@
 
   # https://nixos.wiki/wiki/Printing
   services.printing.enable = true;
+  services.printing.drivers = with pkgs; [ gutenprint gutenprintBin ];
   #services.avahi.enable = true; # <- resulted in hangups on silvio-pc
   # Important to resolve .local domains of printers, otherwise you get an error
   # like  "Impossible to connect to XXX.local: Name or service not known"
