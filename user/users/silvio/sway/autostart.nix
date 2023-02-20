38e3e71a1e7bf2f36dfe24377d126e2355535616
Silvio Ankermann <silvio@booq.org>
2023-02-20 21:43:10 +0100

    user silvio: disable gajim and nc autostart

--- user/users/silvio/sway/autostart.nix
+++ user/users/silvio/sway/autostart.nix
@@ -1,31 +1,3 @@
 { config, lib, pkgs, ... }:
 lib.mkIf (config.booq.gui.enable && config.booq.gui.sway.enable) {
-  systemd.user.services.nextcloud-client = {
-    Unit = {
-      Description = "Nextcloud Client";
-      PartOf = [ "sway-session.target" ];
-      Wants = [ "keepassxc.service" ];
-      After = [ "keepassxc.service" ];
-    };
-    Install.WantedBy = [ "sway-session.target" ];
-    Service = {
-      Environment = "PATH=${config.home.profileDirectory}/bin";
-      ExecStart = "${pkgs.nextcloud-client}/bin/nextcloud --background";
-    };
-  };
-
-  systemd.user.services.gajim = {
-    Unit = {
-      PartOf = [ "sway-session.target" ];
-    };
-
-    Install.WantedBy = [ "sway-session.target" ];
-
-    Service = {
-      ExecStart = ''
-        ${pkgs.gajim}/bin/gajim
-      '';
-      Restart = "on-failure";
-    };
-  };
 }
