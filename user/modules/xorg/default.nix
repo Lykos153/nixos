9ac51c05138b07ec96a933bb4fc0b235649268ca
Silvio Ankermann <silvio@booq.org>
2023-03-15 21:16:11 +0100

    user xorg: enable rofi as program

--- user/modules/xorg/default.nix
+++ user/modules/xorg/default.nix
@@ -21,4 +21,14 @@ lib.mkIf (config.booq.gui.enable && config.booq.gui.xorg.enable) {
     enable = true;
     inactiveInterval = 30;
   };
+
+  programs.rofi = {
+    enable = true;
+    terminal = "${pkgs.alacritty}/bin/alacritty";
+    theme = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/davatorium/rofi/next/themes/arthur.rasi";
+      sha256 = "sha256-2wlR+UURxmk9KvSYm/PmwNKDPC/GV0HcQEH7xDW53k0=";
+    } + ""; #TODO: How to properly convert the set to a string or path?
+  };
+
 }
