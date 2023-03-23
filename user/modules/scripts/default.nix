b71b8a41ccd833797bca32a445ff5302b3484124
Silvio Ankermann <silvio@booq.org>
2023-03-24 00:39:11 +0100

    user scripts: convert to derivations

--- user/modules/scripts/default.nix
+++ user/modules/scripts/default.nix
@@ -1,5 +1,31 @@
 { config, lib, pkgs, ... }:
+let
+  rga-fzf = pkgs.writeShellApplication {
+    name = "rga-fzf";
+
+    runtimeInputs = [ pkgs.ripgrep-all pkgs.fzf ];
+
+    text = builtins.readFile ./bin/rga-fzf;
+  };
+  zsh_history_fix = pkgs.writeShellApplication {
+    name = "zsh_history_fix";
+
+    runtimeInputs = [ pkgs.binutils ];
+
+    text = builtins.readFile ./bin/zsh_history_fix;
+  };
+  list-iommu-groups = pkgs.writeShellApplication {
+    name = "list-iommu-groups";
+
+    runtimeInputs = [ pkgs.pciutils ];
+
+    text = builtins.readFile ./bin/list-iommu-groups;
+  };
+in
 {
-  home.file.".local/bin".source = ./bin;
-  home.file.".local/bin".recursive = true;
+  home.packages = [
+    rga-fzf
+    zsh_history_fix
+    list-iommu-groups
+  ];
 }
