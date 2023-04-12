48f0a6c2f3063ab1c7fa73841afba3d8517ed489
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2023-04-12 09:17:23 +0200

    Use nix-pre-commit and add stylish-haskell hook

--- flake.nix
+++ flake.nix
@@ -1,21 +1,60 @@
 {
-  description = "A collection of flake templates";
-
   inputs = {
     direnv.url = "github:nix-community/nix-direnv";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs";
+    nix-pre-commit.url = "github:jmgilman/nix-pre-commit";
   };
 
-  outputs = { self, direnv }: {
+  outputs = { self, nixpkgs, flake-utils, nix-pre-commit, direnv }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
 
-    templates = {
-      # TODO: Check what https://github.com/jonringer/nix-template does
-      pythonenv = {
-        path = ./templates/pythonenv;
-        description = "Flake to setup python virtualenv with direnv";
-      };
+        config = {
+          repos = [
+            {
+              repo = "https://github.com/pre-commit/pre-commit-hooks";
+              rev = "v4.4.0";
+              hooks = [
+                { id = "trailing-whitespace"; }
+                { id = "end-of-file-fixer"; }
+                { id = "mixed-line-ending"; }
+              ];
+            }
+            {
+              repo = "local";
+              hooks = [
+                {
+                  id = "stylish-haskell";
+                  name = "stylish-haskell";
+                  description = "Haskell code prettifier.";
+                  entry = "${pkgs.stylish-haskell}/bin/stylish-haskell -i";
+                  language = "system";
+                  files = "\\.l?hs$";
+                }
+              ];
+            }
+          ];
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          shellHook = (nix-pre-commit.lib.${system}.mkConfig {
+            inherit pkgs config;
+          }).shellHook;
+        };
 
-      direnv = direnv.defaultTemplate;
+        templates = {
+          # TODO: Check what https://github.com/jonringer/nix-template does
+          pythonenv = {
+            path = ./templates/pythonenv;
+            description = "Flake to setup python virtualenv with direnv";
+          };
 
-    };
-  };
+          direnv = direnv.defaultTemplate;
+
+        };
+      });
 }
