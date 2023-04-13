c573544144102c6ee607fff815bc7c370307ea82
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2023-04-13 18:58:07 +0200

    fixup! user xorg: use polybar

--- user/modules/polybar/default.nix
+++ user/modules/polybar/default.nix
@@ -1,21 +1,19 @@
 { config, pkgs, lib, ... }:
+let
+  mainBar = builtins.readFile ./bar.ini;
+in
 lib.mkIf (config.booq.gui.enable && config.booq.gui.xorg.enable) {
-  let
-    mainBar = builtins.readFile ./bar.ini;
-  in
-  {
-    services.polybar = {
-      enable = true;
-      package = pkgs.polybar.override {
-        pulseSupport = true;
-      };
-      config = ./config.ini;
-      extraConfig = mainBar;
-      script = ''
-        for m in $(polybar --list-monitors | ${pkgs.coreutils}/bin/cut -d":" -f1); do
-          MONITOR=$m polybar --reload main & disown
-        done
-      '';
+  services.polybar = {
+    enable = true;
+    package = pkgs.polybar.override {
+      pulseSupport = true;
     };
-  }
+    config = ./config.ini;
+    extraConfig = mainBar;
+    script = ''
+      for m in $(polybar --list-monitors | ${pkgs.coreutils}/bin/cut -d":" -f1); do
+        MONITOR=$m polybar --reload main & disown
+      done
+    '';
+  };
 }
