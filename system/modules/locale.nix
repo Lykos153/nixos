83672b2e9b5880df69adbd3abee660510dcb2526
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2023-04-19 13:46:49 +0200

    system xkb: add mine layout

--- system/modules/locale.nix
+++ system/modules/locale.nix
@@ -1,3 +1,4 @@
+{ pkgs, ...}:
 {
   time.timeZone = "Europe/Berlin";
 
@@ -5,5 +6,14 @@
   console = {
     font = "Lat2-Terminus16";
     keyMap = "de";
+  };
+
+  services.xserver.extraLayouts.mine = {
+    description = "The latest neo variant";
+    languages   = [ "mine" ];
+    symbolsFile = pkgs.fetchurl {
+      url = "https://dl.neo-layout.org/mine";
+      sha256 = "sha256-9zx3Iei4uSUZahlBhtgsuWI0BKMbr8ukVC4PQBlqoyw=";
+    };
   };
 }
