04b8d1d2e0104012b9a6fc517fc331539ec78cce
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2023-04-26 10:39:24 +0200

    user sa: add aopass

--- user/users/sa/zsh.nix
+++ user/users/sa/zsh.nix
@@ -3,6 +3,7 @@
   programs.zsh = {
     shellAliases = {
       opsipass = "PASSWORD_STORE_DIR=$HOME/cah/opsi-data/opsi-pass pass";
+      aopass = "PASSWORD_STORE_DIR=$HOME/cah/aoterra_admin_passwords pass";
     };
   };
 }
