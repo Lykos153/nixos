905872a7d40e397535787d09bc2599623775e5a3
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2023-05-04 12:37:37 +0200

    user autorandr: home with kvm

--- user/modules/autorandr.nix
+++ user/modules/autorandr.nix
@@ -1,3 +1,4 @@
+{ config, lib, nixosConfig, pkgs, ... }:
 let
   laptop = "00ffffffffffff0006af3d1000000000001a0104951f117802c32592575a942a22505400000001010101010101010101010101010101143780b2703828403064310035ad100000180000000f0000000000000000000000000020000000fe0041554f0a202020202020202020000000fe004231343048414b30312e30200a00bd";
   samsung_syncmaster = "00ffffffffffff004c2d87053736535a17150103803420782aee91a3544c99260f5054230800a9408180814081009500b30001010101283c80a070b023403020360006442100001a000000fd00323f1e5111000a202020202020000000fc0053796e634d61737465720a2020000000ff00484d42423630323436320a202001ba02010400023a801871382d40582c450006442100001e023a80d072382d40102c458006442100001e011d007251d01e206e28550006442100001e011d00bc52d01e20b828554006442100001e8c0ad090204031200c4055000644210000188c0ad08a20e02d10103e960006442100001800000000000000000000000000000096";
@@ -10,9 +11,30 @@ let
     [ 0.0        (invert x)  0.0 ]
     [ 0.0        0.0         1.0 ]
   ];
-
+  home-kvm = on: {
+    eDP-1 = {
+      enable = true;
+      mode = "1920x1080";
+      position = "720x1200";
+      rate = "60.01";
+      transform = scale_to_transform 1.6;
+    };
+    DP-2-1 = lib.mkIf on {
+      enable = true;
+      mode = "1920x1200";
+      position = "0x0";
+      rate = "59.95";
+    };
+    DP-2-2 = {
+      enable = on;
+      primary = true;
+      mode = "1920x1080";
+      position = "1920x0";
+      rate = "59.95";
+      rotate = "left";
+    };
+  };
 in
-{ config, lib, nixosConfig, pkgs, ... }:
 lib.mkIf (config.booq.gui.enable && config.booq.gui.xorg.enable) {
   programs.autorandr = {
     enable = true;
@@ -23,29 +45,14 @@ lib.mkIf (config.booq.gui.enable && config.booq.gui.xorg.enable) {
           DP-2-2 = benq;
           eDP-1 = laptop;
         };
-        config = {
-          eDP-1 = {
-            enable = true;
-            mode = "1920x1080";
-            position = "720x1080";
-            rate = "60.01";
-            transform = scale_to_transform 1.6;
-          };
-          DP-2-1 = {
-            enable = true;
-            mode = "1920x1200";
-            position = "1920x0";
-            rate = "59.95";
-            rotate = "left";
-          };
-          DP-2-2 = {
-            enable = true;
-            primary = true;
-            mode = "1920x1080";
-            position = "0x0";
-            rate = "59.95";
-          };
+        config = home-kvm true;
+      };
+      home-no-kvm = {
+        fingerprint = {
+          DP-2-2 = benq;
+          eDP-1 = laptop;
         };
+        config = home-kvm false;
       };
       mobile = {
         fingerprint = {
