37ab6bed9df0da4e0c879cae3631bc2df3b77d40
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2023-05-23 12:11:06 +0200

    user helix: add language servers

--- user/modules/helix/default.nix
+++ user/modules/helix/default.nix
@@ -2,6 +2,21 @@
 {
   home.packages = [
     pkgs.helix
+
+    # Language servers
+    # https://docs.helix-editor.com/lang-support.html
+    pkgs.nil # Nix
+    pkgs.taplo # toml
+    pkgs.yaml-language-server
+    pkgs.python3Packages.python-lsp-server # Python
+    pkgs.marksman # Markdown
+    pkgs.gopls # Go
+    #pkgs.docker-langserver
+    #pkgs.bash-language-server
+    pkgs.terraform-ls
+    # pkgs.vscode-json-language-server
+    pkgs.jsonnet-language-server
+    pkgs.rust-analyzer
   ];
   xdg.configFile."helix/config.toml".source = ./config.toml;
 }
