92f895beb575d2002bb9a004d914bc06d1871c34
Silvio Ankermann <silvio@booq.org>
2023-06-27 00:37:21 +0200

    add sops to env; move .sops.yaml to top

--- flake.nix
+++ flake.nix
@@ -2,11 +2,29 @@
   description = "A collection of flake templates";
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
     direnv.url = "github:nix-community/nix-direnv";
   };
 
-  outputs = { self, direnv }: {
+  outputs = { self
+    , nixpkgs
+    , flake-utils
+    , direnv
+  }:
 
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              sops
+            ];
+          };
+        }) // {
     templates = {
       # TODO: Check what https://github.com/jonringer/nix-template does
       pythonenv = {
