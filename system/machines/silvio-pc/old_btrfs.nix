0a8f193950232d148d7ad7772c4e378b41e440d0
Silvio Ankermann <silvio@booq.org>
2023-07-04 22:59:56 +0200

    system silvio-pc: open old btrfs with key file

--- system/machines/silvio-pc/old_btrfs.nix
+++ system/machines/silvio-pc/old_btrfs.nix
@@ -5,7 +5,16 @@
       options = [ "subvol=/" "ro" ];
     };
 
-  boot.initrd.luks.devices."arch-root".device = "/dev/disk/by-uuid/b474463d-7d93-4abf-95cd-35db2f9a6490";
-  boot.initrd.luks.devices."arch-root2".device = "/dev/disk/by-uuid/efae7e7f-5c77-4957-9a75-f2862e620d15";
-  boot.initrd.luks.devices."arch-root3".device = "/dev/disk/by-uuid/d55b7110-02b2-47fd-9441-bf53c67eeccc";
+  boot.initrd.luks.devices."arch-root" = {
+    device = "/dev/disk/by-uuid/b474463d-7d93-4abf-95cd-35db2f9a6490";
+    keyFile = "/sysroot/persist/passwords/luks_old_btrfs";
+  };
+  boot.initrd.luks.devices."arch-root2" = {
+    device = "/dev/disk/by-uuid/efae7e7f-5c77-4957-9a75-f2862e620d15";
+    keyFile = "/sysroot/persist/passwords/luks_old_btrfs";
+  };
+  boot.initrd.luks.devices."arch-root3" = {
+    device = "/dev/disk/by-uuid/d55b7110-02b2-47fd-9441-bf53c67eeccc";
+    keyFile = "/sysroot/persist/passwords/luks_old_btrfs";
+  };
 }
