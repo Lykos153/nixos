3c8e7a0be870721ca0515648cc99a82dd103d7ed
Silvio Ankermann <silvio@booq.org>
2023-07-05 02:22:26 +0200

    system: Add comments

--- system/machines/silvio-pc/impermanence.nix
+++ system/machines/silvio-pc/impermanence.nix
@@ -1,4 +1,5 @@
 {
+  # some more paths maybe: https://www.reddit.com/r/NixOS/comments/ymq9s2/comment/iv6cl56/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
   environment.persistence."/persist" = {
     hideMounts = true;
     directories = [
