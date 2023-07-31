43cf3a3dee622a5b37ccfb872a62ddd9c1c17833
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2023-07-31 22:20:17 +0200

    user k8s: add more k8s tools

--- user/modules/kubernetes/default.nix
+++ user/modules/kubernetes/default.nix
@@ -1,10 +1,14 @@
 { pkgs, ... }:
 {
   home.packages = [
-    (pkgs.kubectl.withKrewPlugins (plugins: [
-      plugins.node-shell
-      plugins.get-all
+    (pkgs.kubectl.withKrewPlugins (plugins: with plugins; [
+      node-shell
+      get-all
+      example
+      cnpg
     ]))
+    pkgs.kustomize
+    pkgs.fluxcd
     pkgs.k9s
     pkgs.kubernetes-helm
   ];
