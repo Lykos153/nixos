2c9ff692ec9b412a8f7a9f20736282133faa45c6
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2023-07-31 22:22:46 +0200

    user sa: add opsi-handbook service

--- user/users/sa/default.nix
+++ user/users/sa/default.nix
@@ -14,6 +14,7 @@
     rocketchat-desktop
     konversation
     mumble
+    git-crypt
   ];
   imports = [
     ./autostart.nix
@@ -24,5 +25,6 @@
     ./mum-rs.nix
     ./sops.nix
     ./yk8s
+    ./opsi-handbook.nix
   ];
 }
