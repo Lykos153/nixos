633618465b0882d817ecf2f9a8c6e5942428d209
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2023-08-04 17:09:25 +0200

    user: modularize mail

---
+++ user/modules/mail.nix
@@ -0,0 +1,40 @@
+{ config, lib, pkgs, ... }:
+
+{
+  options.booq.mail.enable = lib.mkEnableOption "mail";
+
+  config = lib.mkIf config.booq.mail.enable {
+    programs.mbsync.enable = true;
+    services.imapnotify.enable = true;
+    programs.msmtp.enable = true;
+    programs.notmuch = {
+      enable = true;
+      hooks = {
+        preNew = "mbsync --all";
+      };
+    };
+    programs.neomutt = {
+      enable = true;
+      macros = [
+        {
+          map = [ "index" "pager" ];
+          key = "\\cb";
+          action = "<pipe-message> ${pkgs.urlscan}/bin/urlscan<Enter>"; #call urlscan to extract URLs out of a message
+        }
+        {
+          map = [ "attach" "compose" ];
+          key = "\\cb";
+          action = "<pipe-entry> ${pkgs.urlscan}/bin/urlscan<Enter>"; #call urlscan to extract URLs out of a message
+        }
+      ];
+      extraConfig = ''
+        auto_view text/html
+        set mailcap_path = ${config.xdg.configHome + "/neomutt/mailcap"}
+      '';
+    };
+    xdg.configFile."neomutt/mailcap".text = ''
+      text/html; ${pkgs.firefox}/bin/firefox %s;
+      text/html; ${pkgs.lynx}/bin/lynx -assume_charset=%{charset} -display_charset=utf-8 -dump %s; nametemplate=%s.html; copiousoutput
+    '';
+  };
+}
