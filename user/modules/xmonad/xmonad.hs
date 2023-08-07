346f64f1f4dc2d4ab5e94ceac17ddcbae2a24add
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2023-08-07 12:33:09 +0200

    user xmonad: add mod+t for terminal

--- user/modules/xmonad/xmonad.hs
+++ user/modules/xmonad/xmonad.hs
@@ -100,6 +100,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
   [
     -- launch a terminal
     ((modm,                 xK_Return), spawn $ XMonad.terminal conf)
+    , ((modm,                 xK_t), spawn $ XMonad.terminal conf)
 
     -- launch dmenu
     , ((modm,               xK_d     ), spawn Tools.dmenu)
@@ -162,7 +163,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm,               xK_l     ), sendMessage Expand)
 
     -- Push window back into tiling
-    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
+    , ((modm .|. shiftMask, xK_t     ), withFocused $ windows . W.sink)
 
     -- Increment the number of windows in the master area
     , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
