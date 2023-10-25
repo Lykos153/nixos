93e6d46723385e11564c42cd05dee3eb4bf996c8
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2023-10-25 13:08:56 +0200

    user sa: add service for bugwarrior

--- user/users/sa/bugwarrior.nix
+++ user/users/sa/bugwarrior.nix
@@ -1,8 +1,14 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: let
+  bugwarriorrc = "bugwarrior/bugwarrior.toml";
+in {
   home.packages = with pkgs; [
     lykos153.bugwarrior
   ];
-  xdg.configFile."bugwarrior/bugwarrior.toml".source = let
+  xdg.configFile."${bugwarriorrc}".source = let
     gitlab = {
       service = "gitlab";
       host = "gitlab.com";
@@ -27,6 +33,12 @@
         annotation_links = true;
         annotation_comments = false;
       };
+      ## Doesn't yet work because of gobject introspection
+      # notifications = {
+      #   notifications = true;
+      #   backend = "gobject";
+      #   only_on_new_tasks = true;
+      # };
       redmine = {
         service = "redmine";
         url = "https://redmine.cloudandheat.com";
@@ -67,4 +79,32 @@
         membership = true;
       };
     };
+  systemd.user.timers."bugwarrior" = {
+    Install.WantedBy = ["timers.target"];
+    Timer = {
+      OnCalendar = "*:0/15";
+      Persistent = "true";
+      Unit = "bugwarrior.service";
+    };
+  };
+
+  systemd.user.services."bugwarrior" = {
+    Service = {
+      Environment = let
+        bugwarrior-env = pkgs.buildEnv {
+          name = "bugwarrior-env";
+          paths = with pkgs; [
+            lykos153.bugwarrior
+            taskwarrior
+            pass
+          ];
+        };
+      in [
+        "PATH=${bugwarrior-env}/bin"
+        "BUGWARRIORRC=${config.xdg.configHome}/${bugwarriorrc}"
+      ];
+      ExecStart = "${pkgs.lykos153.bugwarrior}/bin/bugwarrior pull";
+      Type = "simple";
+    };
+  };
 }
