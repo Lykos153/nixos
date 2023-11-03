a4616e47f6abe8d5b0e8be3ac0680630fe3e077b
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2023-11-03 17:21:32 +0100

    system silvio-ch: passwordless sudo because docker

--- system/machines/silvio-ch/docker.nix
+++ system/machines/silvio-ch/docker.nix
@@ -4,6 +4,10 @@
   users.users.sa = {
     extraGroups = ["docker"];
   };
+  users.users.silvio = {
+    extraGroups = ["docker"];
+  };
+  security.sudo.wheelNeedsPassword = false; # docker access means root anyway
   environment.persistence."/persist" = {
     hideMounts = true;
     directories = [
