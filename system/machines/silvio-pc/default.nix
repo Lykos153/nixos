df5bc82ecf465732d982b8394ca927b62295cfe8
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-01-12 12:28:58 +0100

    system silvio-pc: enable zram

--- system/machines/silvio-pc/default.nix
+++ system/machines/silvio-pc/default.nix
@@ -9,6 +9,7 @@
   booq.gaming.enable = true;
   booq.impermanence.enable = true;
   # TODO: luks+yubikey+secureboot https://www.reddit.com/r/NixOS/comments/xrgszw/comment/iqf1gps/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+  zramSwap.enable = true;
   imports = [
     ./hardware-configuration.nix
     ./old_btrfs.nix
