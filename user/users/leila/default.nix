59f407b5e8933b430b5f7785f7b2cb68c4c28ccd
Leila Höll <leila@booq.org>
2024-01-15 22:47:43 +0100

    user leila: use qgis-ltr

--- user/users/leila/default.nix
+++ user/users/leila/default.nix
@@ -17,7 +17,7 @@
     darktable
     solaar # TODO udev rules https://search.nixos.org/packages?channel=unstable&show=solaar&from=0&size=50&sort=relevance&type=packages&query=solaar
     clementine
-    qgis
+    qgis-ltr
 
     tdesktop # telegram
 
