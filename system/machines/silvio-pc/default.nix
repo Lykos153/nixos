590b014d6b15504326783fe0d65afe02cb163b71
Silvio Ankermann <silvio@booq.org>
2024-01-21 03:14:17 +0100

    system silvio-pc: enable bcachefs

--- system/machines/silvio-pc/default.nix
+++ system/machines/silvio-pc/default.nix
@@ -10,6 +10,7 @@
   booq.impermanence.enable = true;
   # TODO: luks+yubikey+secureboot https://www.reddit.com/r/NixOS/comments/xrgszw/comment/iqf1gps/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
   zramSwap.enable = true;
+  boot.supportedFilesystems = ["bcachefs"];
   imports = [
     ./hardware-configuration.nix
     ./hdd.nix
