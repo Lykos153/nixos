f489f35712f60d5e59d0e5f0a71a7a6e98199455
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-01-30 14:15:01 +0100

    Revert "user sa mail: use hm fork for signature"
    This reverts commit 6785b928fc09d0417d3203eab54cea95867eb2a0.

--- user/flake.nix
+++ user/flake.nix
@@ -4,8 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     # nixpkgs-master.url = "github:nixos/nixpkgs/master";
-    # home-manager.url = "github:nix-community/home-manager";
-    home-manager.url = "github:lykos153/home-manager/feature/thunderbird-sig";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.url = "github:Mic92/sops-nix";
     mynur.url = "github:Lykos153/nur-packages";
