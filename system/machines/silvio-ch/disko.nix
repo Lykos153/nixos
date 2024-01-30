ea1fd036df6a0253c0756f1e76574e17d09aab3a
Silvio Ankermann <silvio@booq.org>
2024-01-30 11:22:28 +0100

    system silvio-ch: reprovision

--- system/machines/silvio-ch/disko.nix
+++ system/machines/silvio-ch/disko.nix
@@ -1,73 +1,32 @@
 {config, ...}: {
   disko.devices = {
     disk.nvme-samsung = {
-      device = "/dev/disk/by-id/nvme-SAMSUNG_MZVLB512HAJQ-000L7_S3TNNE0JC78861";
+      device = "/dev/disk/by-id/nvme-SKHynix_HFS001TEJ4X113N_4SC8N51931OA08M50_1";
       type = "disk";
       content = {
-        type = "table";
-        format = "gpt";
-        partitions = [
-          {
-            name = "ESP";
-            start = "2048";
-            end = "2099199";
-            bootable = true;
+        type = "gpt";
+        partitions = {
+          ESP = {
+            label = "ESP";
+            size = "500";
+            type = "EF00";
             content = {
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
             };
-          }
-          {
-            name = "luks";
-            start = "2099200";
-            end = "945817599";
+          };
+          rootfs = {
+            label = "rootfs";
+            size = "200000";
             content = {
-              type = "luks";
-              name = "crypted";
-              extraOpenArgs = ["--allow-discards"];
-              # if you want to use the key for interactive login be sure there is no trailing newline
-              # keyFile = "/dev/shm/secret.key";
-              content = {
-                type = "btrfs";
-                extraArgs = ["-f"]; # Override existing partition
-                subvolumes = {
-                  "/@" = {
-                    mountpoint = "${config.booq.impermanence.persistRoot}";
-                    mountOptions = ["compress=zstd"];
-                  };
-                  "/@home" = {
-                    mountpoint = "/home";
-                    mountOptions = ["compress=zstd"];
-                  };
-                  "/@var-tmp" = {
-                    mountpoint = "/var/tmp";
-                    mountOptions = [
-                      "mode=777"
-                    ];
-                  };
-                  "/@nix" = {
-                    mountpoint = "/nix";
-                    mountOptions = ["compress=zstd" "noatime"];
-                  };
-                };
-              };
+              type = "filesystem";
+              format = "bcachefs";
+              mountpoint = "/nix";
+              # extraOpenArgs = ["-osubvol=nix"];
             };
-          }
-          {
-            name = "luks";
-            start = "945817600";
-            end = "100%";
-            content = {
-              type = "luks";
-              name = "swap";
-              extraOpenArgs = ["--allow-discards"];
-              content = {
-                type = "swap";
-              };
-            };
-          }
-        ];
+          };
+        };
       };
     };
     nodev."/" = {
@@ -87,6 +46,6 @@
       ];
     };
   };
-  fileSystems."${config.booq.impermanence.persistRoot}".neededForBoot = true;
+  # fileSystems."/nix".neededForBoot = true;
   boot.kernel.sysctl."vm.swappiness" = 0; # Use swap only for hibernate (SSD)
 }
