c25c293d9912bf097db0bd98caa50d3684589041
Silvio Ankermann <silvio@booq.org>
2024-02-01 22:32:59 +0100

    Upgrade

--- system/modules/locale.nix
+++ system/modules/locale.nix
@@ -7,14 +7,16 @@
     keyMap = "de";
   };
 
-  services.xserver.layout = "de";
+  services.xserver.xkb = {
+    layout = "de";
 
-  services.xserver.extraLayouts.mine = {
-    description = "The latest neo variant";
-    languages = ["mine"];
-    symbolsFile = pkgs.fetchurl {
-      url = "https://dl.neo-layout.org/mine";
-      sha256 = "sha256-9zx3Iei4uSUZahlBhtgsuWI0BKMbr8ukVC4PQBlqoyw=";
+    extraLayouts.mine = {
+      description = "The latest neo variant";
+      languages = ["mine"];
+      symbolsFile = pkgs.fetchurl {
+        url = "https://dl.neo-layout.org/mine";
+        sha256 = "sha256-9zx3Iei4uSUZahlBhtgsuWI0BKMbr8ukVC4PQBlqoyw=";
+      };
     };
   };
 }
