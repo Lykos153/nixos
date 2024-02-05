68bd819bdde2c7085c67b146710917e4eaaf80c3
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-02-05 14:15:34 +0100

    system silvio-pc: back to pulseaudio

--- system/machines/silvio-pc/default.nix
+++ system/machines/silvio-pc/default.nix
@@ -5,7 +5,7 @@
   modulesPath,
   ...
 }: {
-  booq.audio = "pipewire";
+  # booq.audio = "pipewire";
   booq.gaming.enable = true;
   booq.impermanence.enable = true;
   # TODO: luks+yubikey+secureboot https://www.reddit.com/r/NixOS/comments/xrgszw/comment/iqf1gps/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
