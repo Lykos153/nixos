3d201a99a64fb82c4a4d2f4ff4cfc55a71fa3531
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-02-05 16:09:32 +0100

    nix-fmt everything

--- templates/go/flake.nix
+++ templates/go/flake.nix
@@ -3,7 +3,11 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
--- templates/pythonenv/flake.nix
+++ templates/pythonenv/flake.nix
@@ -6,16 +6,20 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in rec {
       packages = {
-        pythonEnv = pkgs.python3.withPackages(
-          ps: with ps; [
-            virtualenv
-          ]
+        pythonEnv = pkgs.python3.withPackages (
+          ps:
+            with ps; [
+              virtualenv
+            ]
         );
       };
 
