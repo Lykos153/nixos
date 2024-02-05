ef0c80222cd3704b4cf563a472d534865c2381aa
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-02-05 21:32:17 +0100

    user talon: base on pokey

--- user/modules/talon/default.nix
+++ user/modules/talon/default.nix
@@ -36,18 +36,6 @@ in {
 
     home.file.".talon/user/cursorless-talon".source = pkgs.repos.cursorless-talon;
     home.file.".talon/user/rango".source = pkgs.repos.rango;
-    home.file.".talon/user/cursorless-settings".source = ./cursorless-settings;
-    home.file.".talon/user/talon-community".source = pkgs.stdenv.mkDerivation {
-      name = "talon-community";
-      src = ./.;
-      buildPhase = "true";
-      installPhase = ''
-        mkdir $out
-        for f in ${pkgs.repos.talon-community}/*; do
-          ln -s $f $out/$(basename $f)
-        done
-        cp -r ./talon-settings $out/settings
-      '';
-    };
+    home.file.".talon/user/talon-community".source = pkgs.repos.talon-community;
   };
 }
