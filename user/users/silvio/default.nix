77e3a4a399bf676689f4683428f5ca1a800f8756
Silvio Ankermann <silvio@booq.org>
2024-02-12 02:02:30 +0100

    user silvio: enable talon

--- user/users/silvio/default.nix
+++ user/users/silvio/default.nix
@@ -2,6 +2,7 @@
   booq.gui.enable = true;
   booq.gui.sway.enable = false;
   booq.gui.xmonad.enable = true;
+  booq.talon.enable = true;
   programs.git.userEmail = "silvio@booq.org";
   programs.git.userName = "Silvio Ankermann";
 
