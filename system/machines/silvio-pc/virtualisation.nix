66d3d5d2c033bad1a34e58161038b19ee94de28f
Silvio Ankermann <silvio@booq.org>
2024-02-22 21:25:50 +0100

    tmp. disable acs override

--- system/machines/silvio-pc/virtualisation.nix
+++ system/machines/silvio-pc/virtualisation.nix
@@ -16,26 +16,26 @@
   };
 
   # workaround for dysfunctional option upstream
-  boot.kernelParams = [
-    "pcie_acs_override=downstream,multifunction"
-    "pci=nomsi"
-  ];
-  boot.kernelPatches = [
-    {
-      name = "add-acs-overrides";
-      patch = pkgs.fetchurl {
-        name = "add-acs-overrides.patch";
-        url = "https://aur.archlinux.org/cgit/aur.git/plain/1001-6.6.7-add-acs-overrides.patch?h=linux-vfio&id=3d7255075f74ae27d4bddfc9c18f8e434e8255d3";
-        sha256 = "sha256-80KYa9J5gMlslSsN2BA9PiGpQth/GN8TCPqzcOIAEPs=";
-      };
-    }
-    {
-      name = "i915-vga-arbiter";
-      patch = pkgs.fetchurl {
-        name = "i915-vga-arbiter.patch";
-        url = "https://aur.archlinux.org/cgit/aur.git/plain/1002-6.6.7-i915-vga-arbiter.patch?h=linux-vfio&id=3d7255075f74ae27d4bddfc9c18f8e434e8255d3";
-        sha256 = "sha256-KjxzLU1hpjHJiyo+Svsfpdv4vlxDUZsqWdDmUXDJ2Ns=";
-      };
-    }
-  ];
+  # boot.kernelParams = [
+  #   "pcie_acs_override=downstream,multifunction"
+  #   "pci=nomsi"
+  # ];
+  # boot.kernelPatches = [
+  #   {
+  #     name = "add-acs-overrides";
+  #     patch = pkgs.fetchurl {
+  #       name = "add-acs-overrides.patch";
+  #       url = "https://aur.archlinux.org/cgit/aur.git/plain/1001-6.6.7-add-acs-overrides.patch?h=linux-vfio&id=3d7255075f74ae27d4bddfc9c18f8e434e8255d3";
+  #       sha256 = "sha256-80KYa9J5gMlslSsN2BA9PiGpQth/GN8TCPqzcOIAEPs=";
+  #     };
+  #   }
+  #   {
+  #     name = "i915-vga-arbiter";
+  #     patch = pkgs.fetchurl {
+  #       name = "i915-vga-arbiter.patch";
+  #       url = "https://aur.archlinux.org/cgit/aur.git/plain/1002-6.6.7-i915-vga-arbiter.patch?h=linux-vfio&id=3d7255075f74ae27d4bddfc9c18f8e434e8255d3";
+  #       sha256 = "sha256-KjxzLU1hpjHJiyo+Svsfpdv4vlxDUZsqWdDmUXDJ2Ns=";
+  #     };
+  #   }
+  # ];
 }
