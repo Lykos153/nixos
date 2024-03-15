f386916d7fa07a90c77ee117791ace9dc8d89da3
Silvio Ankermann <silvio@booq.org>
2024-03-16 00:35:57 +0100

    system silvio-pc: enable iommu

--- system/machines/silvio-pc/virtualisation.nix
+++ system/machines/silvio-pc/virtualisation.nix
@@ -4,6 +4,8 @@
     libvirtUsers = ["silvio" "sa"];
   };
 
+  boot.kernelParams = ["intel_iommu=on"];
+
   # Blacklist nvidia because it's annoying.
   # TODO: pcie passthrough using ie. https://github.com/CRTified/nur-packages
   boot.blacklistedKernelModules = ["nvidia" "nouveau"];
