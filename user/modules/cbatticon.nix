bd9cfbc7ff6a72d9fa6faa087fd9afc5f6e9312f
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-03-20 11:39:09 +0100

    user cbatticon: timeout dunst notification after 10s

--- user/modules/cbatticon.nix
+++ user/modules/cbatticon.nix
@@ -1,4 +1,9 @@
 {
   services.cbatticon.enable = true;
   services.cbatticon.batteryId = "BAT0";
+  services.dunst.settings.cbatticon = {
+    appname = "cbatticon";
+    msg_urgency = "normal";
+    override_dbus_timeout = 10;
+  };
 }
