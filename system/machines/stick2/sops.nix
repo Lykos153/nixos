1acb2b767231290138432d9cce74769cb2a65a55
Silvio Ankermann <silvio@booq.org>
2024-04-03 15:47:20 +0200

    fixup! system impermanence: make persistRoot configurable

--- system/machines/stick2/sops.nix
+++ system/machines/stick2/sops.nix
@@ -1,7 +1,7 @@
-{
+{config, ...}: {
   # needs to be the actual path, because sops is loaded before impermanence
   sops.age.sshKeyPaths = [];
-  sops.age.keyFile = "/persist/var/lib/sops-nix/age.key";
+  sops.age.keyFile = "${config.booq.impermanence.persistRoot}/var/lib/sops-nix/age.key";
   sops.age.generateKey = true;
   sops.gnupg.sshKeyPaths = [];
 }
