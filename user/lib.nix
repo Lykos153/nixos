8a57a2f5a47204b8913ce6ea6bce83d65b8dc228
Silvio Ankermann <silvio@booq.org>
2024-04-08 23:28:27 +0200

    user lib: add mkMinConfig

--- user/lib.nix
+++ user/lib.nix
@@ -2,14 +2,13 @@
   home-manager,
   nixpkgs,
 }: rec {
-  mkConfig = {
+  mkMinConfig = {
+    username,
+    hostname,
     genOverlays,
     extraModules,
-    hostConfig,
-    userConfig,
+    extraArgs,
   }: let
-    username = userConfig.name;
-    hostname = hostConfig.config.system.name;
     userpath = ./users + "/${username}";
     hostpath = userpath + "/${hostname}";
 
@@ -21,41 +20,65 @@
       if builtins.pathExists hostpath
       then [hostpath]
       else [];
-    pkgs = import nixpkgs rec {
-      system = hostConfig.pkgs.stdenv.hostPlatform.system;
-      overlays = genOverlays system;
-    };
   in
-    home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      modules =
-        [
-          ./home.nix
+    home-manager.lib.homeManagerConfiguration ({
+        modules =
+          [
+            ./home.nix
+            {
+              home = {
+                stateVersion = "22.05";
+              };
+              booq = {
+                nix-index = {
+                  enable = true;
+                  nixpkgs-path = "${nixpkgs}";
+                };
+              };
+            }
+            {
+              home.sessionVariables.NIX_PATH = "nixpkgs=${nixpkgs}";
+              # workaround because the above doesnt seem to work in xorg https://github.com/nix-community/home-manager/issues/1011#issuecomment-1365065753
+              programs.zsh.initExtra = ''
+                export NIX_PATH="nixpkgs=${nixpkgs}"
+              '';
+            }
+          ]
+          ++ extraModules
+          ++ userlist
+          ++ hostlist;
+      }
+      // extraArgs);
+
+  mkConfig = {
+    genOverlays,
+    extraModules,
+    hostConfig,
+    userConfig,
+  }: let
+    username = userConfig.name;
+    hostname = hostConfig.config.system.name;
+  in
+    mkMinConfig {
+      inherit genOverlays username hostname;
+      extraArgs = {
+        pkgs = import nixpkgs rec {
+          system = hostConfig.pkgs.stdenv.hostPlatform.system;
+          overlays = genOverlays system;
+        };
+      };
+      extraModules =
+        extraModules
+        ++ [
           {
             home = {
               homeDirectory = userConfig.home;
               username = username;
-              stateVersion = "22.05";
-            };
-            booq = {
-              nix-index = {
-                enable = true;
-                nixpkgs-path = "${nixpkgs}";
-              };
             };
           }
-          {
-            home.sessionVariables.NIX_PATH = "nixpkgs=${nixpkgs}";
-            # workaround because the above doesnt seem to work in xorg https://github.com/nix-community/home-manager/issues/1011#issuecomment-1365065753
-            programs.zsh.initExtra = ''
-              export NIX_PATH="nixpkgs=${nixpkgs}"
-            '';
-          }
-        ]
-        ++ extraModules
-        ++ userlist
-        ++ hostlist;
+        ];
     };
+
   mkConfigs = {
     nixosConfigurations,
     modules,
