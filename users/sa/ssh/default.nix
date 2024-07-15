50381c7ad9bcc5da241e0feb197aae419673b45b
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-07-15 15:59:08 +0200

    user sa ssh: fix merging

--- users/sa/ssh/default.nix
+++ users/sa/ssh/default.nix
@@ -3,11 +3,13 @@
   lib,
   config,
   ...
-}: (builtins.foldl' (acc: v:
-    lib.attrsets.recursiveUpdate acc {
-      sops.secrets.${v}.sopsFile = ./secrets.yaml;
-      sops.secrets."${v}.known_hosts".sopsFile = ./secrets.yaml;
-      home.file.".ssh/config.d/${v}".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/sops-nix/secrets/${v}";
-      home.file.".ssh/${v}.known_hosts".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/sops-nix/secrets/${v}.known_hosts";
-      booq.ssh.extraKnownHostsFiles = [".ssh/${v}.known_hosts"];
-    }) {} ["l1" "ch"])
+}:
+lib.mkMerge (builtins.map (v: let
+  k = "${v}.known_hosts";
+in {
+  sops.secrets.${v}.sopsFile = ./secrets.yaml;
+  sops.secrets.${k}.sopsFile = ./secrets.yaml;
+  home.file.".ssh/config.d/${v}".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/sops-nix/secrets/${v}";
+  home.file.".ssh/${k}".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/sops-nix/secrets/${k}";
+  booq.ssh.extraKnownHostsFiles = ["~/.ssh/${k}"];
+}) ["l1" "ch"])
