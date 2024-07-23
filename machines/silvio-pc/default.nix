313c6b514e085214d221eb80fc217f80014fcd5c
Silvio Ankermann <silvio@booq.org>
2024-07-24 00:29:51 +0200

    machines pc: enable logitech

--- machines/silvio-pc/default.nix
+++ machines/silvio-pc/default.nix
@@ -17,6 +17,9 @@
     local.openvpn-ch.enable = true;
   };
 
+  hardware.logitech.wireless.enable = true;
+  hardware.logitech.wireless.enableGraphical = true;
+
   # TODO: luks+yubikey+secureboot https://www.reddit.com/r/NixOS/comments/xrgszw/comment/iqf1gps/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
   zramSwap.enable = true;
   # TODO evaluate https://github.com/vrmiguel/bustd instead
