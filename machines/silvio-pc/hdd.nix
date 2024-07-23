9b453429f3a3aa12256da388a7e0528cb6fb47ad
Silvio Ankermann <silvio@booq.org>
2024-07-24 00:28:24 +0200

    machines pc: move hdd4 to bcachefs

--- machines/silvio-pc/hdd.nix
+++ machines/silvio-pc/hdd.nix
@@ -8,12 +8,11 @@
     text = ''
       hdd1 /dev/disk/by-uuid/f37a1753-4514-49b1-90a2-235bd1306670 ${config.booq.impermanence.persistRoot}/passwords/luks_hdd luks,nofail
       hdd3 /dev/disk/by-uuid/1affa10c-7564-4a71-847b-daee688c528a ${config.booq.impermanence.persistRoot}/passwords/luks_hdd luks,nofail
-      hdd4 /dev/disk/by-uuid/c0d8d435-a0b7-4910-8789-58b0044bd96f ${config.booq.impermanence.persistRoot}/passwords/luks_hdd luks,nofail
     '';
   };
   fileSystems."/hdd" = {
     device = "/dev/mapper/hdd1";
     fsType = "btrfs";
-    options = ["subvol=/media" "x-systemd.automount"] ++ lib.lists.forEach ["hdd3" "hdd4"] (dev: "x-systemd.requires-mounts-for=/dev/mapper/${dev}");
+    options = ["subvol=/media" "x-systemd.automount"] ++ lib.lists.forEach ["hdd3"] (dev: "x-systemd.requires-mounts-for=/dev/mapper/${dev}");
   };
 }
