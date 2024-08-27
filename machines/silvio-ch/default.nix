599faf5f7c002cd808a60007cabfd91ef11b6aa5
Silvio Ankermann <silvio@booq.org>
2024-08-27 21:30:44 +0200

    machines silvio-ch: enable zram and earlyoom

--- machines/silvio-ch/default.nix
+++ machines/silvio-ch/default.nix
@@ -15,6 +15,14 @@
     libvirtUsers = ["silvio" "sa"];
   };
   booq.local.openvpn-ch.enable = true;
+  zramSwap.enable = true;
+  # TODO evaluate https://github.com/vrmiguel/bustd instead
+  services.earlyoom = {
+    enable = true;
+    freeMemThreshold = 5;
+    freeSwapThreshold = 5;
+    enableNotifications = true;
+  };
   imports = [
     ./hardware-configuration.nix
     ./bootloader.nix
