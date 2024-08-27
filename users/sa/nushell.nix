dfb7f25d24d5085103fae7e91681a70da4ec07f4
Silvio Ankermann <silvio@booq.org>
2024-08-27 21:31:40 +0200

    users sa nushell: update gq function

--- users/sa/nushell.nix
+++ users/sa/nushell.nix
@@ -2,7 +2,7 @@
   programs.nushell = {
     extraConfig = ''
       def --env cl [] {cd (ls ~/clusters/*/* | get name | to text | ${pkgs.skim}/bin/sk)}
-      def --env gq [] {cd (ls ~/ghq/*/*/* | where type == dir | get name | to text | ${pkgs.skim}/bin/sk)}
+      def --env gq [] {cd (fd -H "^.git$" ~/ghq/ | lines | path parse | get parent | to text | ${pkgs.skim}/bin/sk)}
     '';
   };
 }
