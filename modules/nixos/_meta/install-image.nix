945806d5753ae05bc2180e55bac724f70187aa70
Silvio Ankermann <silvio@booq.org>
2024-08-27 21:31:23 +0200

    nixos install-image: enable ntfs and bcachefs

--- modules/nixos/_meta/install-image.nix
+++ modules/nixos/_meta/install-image.nix
@@ -19,6 +19,10 @@ in {
     services.openssh.enable = true;
     security.sudo.wheelNeedsPassword = false;
 
+    # Filesystems from https://github.com/NixOS/nixpkgs/blob/1a88164cc03ff2b7f3e79c042bee932bce7f036d/nixos/modules/profiles/base.nix#L52C2-L53C67
+    # minus ZFS plus NTFS and bcachefs
+    boot.supportedFilesystems = lib.mkForce ["btrfs" "cifs" "f2fs" "jfs" "ntfs" "reiserfs" "vfat" "xfs" "ntfs" "bcachefs"];
+
     # lvm options
     boot.initrd.kernelModules = [
       "dm-snapshot" # when you are using snapshots
