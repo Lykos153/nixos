44ec12e7df9d74415f8abae09e966ce980bf42b0
Silvio Ankermann <silvio@booq.org>
2024-08-31 14:31:19 +0200

    user sa: remove Just-based yk8s

--- users/sa/yk8s/default.nix
+++ users/sa/yk8s/default.nix
@@ -3,8 +3,4 @@
 in {
   xdg.configFile."yaook-k8s/env".source = ./env;
   xdg.configFile."yaook-k8s/envrc.local".source = ./envrc.local;
-  xdg.configFile."${justfilePath}".source = ./Justfile;
-  home.shellAliases = {
-    yk8s = "just --working-directory . --justfile ${config.xdg.configHome}/${justfilePath}";
-  };
 }
