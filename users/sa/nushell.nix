ab2f4ee44395cb9c4049a34f098a640f4a826966
Silvio Ankermann <silvio@booq.org>
2024-08-31 14:31:53 +0200

    user sa nu: add vault commands

--- users/sa/nushell.nix
+++ users/sa/nushell.nix
@@ -3,6 +3,16 @@
     extraConfig = ''
       def --env cl [] {cd (ls ~/clusters/*/* | get name | to text | ${pkgs.skim}/bin/sk)}
       def --env gq [] {cd (fd -H "^.git$" ~/ghq/ | lines | path parse | get parent | to text | ${pkgs.skim}/bin/sk)}
+
+      def --env "vault decode-root" [] {
+        let encoded = (input -s "Encoded token: ")
+        let otp = (input -s "\nOTP: ")
+        $env.VAULT_TOKEN = (vault operator generate-root -decode $encoded -otp $otp)
+      }
+
+      def --env "vault ldap-login" [] {
+        $env.VAULT_TOKEN = (vault login -method=ldap -token-only username=silvio.ankermann)
+      }
     '';
   };
 }
