02cabc8231081c11041a933c62c98949fed04a09
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-09-17 15:14:54 +0200

    fixup! user sa: remove Just-based yk8s

--- users/sa/yk8s/default.nix
+++ users/sa/yk8s/default.nix
@@ -1,6 +1,4 @@
-{config, ...}: let
-  justfilePath = "yaook-k8s/Justfile";
-in {
+{config, ...}: {
   xdg.configFile."yaook-k8s/env".source = ./env;
   xdg.configFile."yaook-k8s/envrc.local".source = ./envrc.local;
 }
