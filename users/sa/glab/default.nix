9a35416aa171cf284b3b84d8f9949b9e49606b78
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-09-24 18:31:58 +0200

    user sa glab: fix permission

--- users/sa/glab/default.nix
+++ users/sa/glab/default.nix
@@ -8,6 +8,7 @@
     glab
   ];
   sops.secrets.glab-config.sopsFile = ./secrets.yaml;
+  sops.secrets.glab-config.mode = "0600";
   xdg.configFile."glab-cli/config.yml".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/sops-nix/secrets/glab-config";
   programs.nushell.extraConfig = ''
     def --wrapped chlab [...args] {
