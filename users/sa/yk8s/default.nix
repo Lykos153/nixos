5db409bf29adea8d7251f0c1b4e37324ab51578c
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-10-01 14:14:11 +0200

    user sa yk8s: add aliases

--- users/sa/yk8s/default.nix
+++ users/sa/yk8s/default.nix
@@ -5,6 +5,10 @@
 }: {
   programs.nushell.extraConfig = ''
     use ${pkgs.repos.yk8s-nu} *
+    alias sshto = yk8s openstack ssh
+    alias getip = yk8s openstack getip
+    alias osl = yk8s openstack list
+    alias osh = yk8s openstack show
   '';
   xdg.configFile."yaook-k8s/env".source = ./env;
   xdg.configFile."yaook-k8s/envrc.local".source = ./envrc.local;
