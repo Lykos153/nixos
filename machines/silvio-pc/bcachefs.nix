88344c683224c408a6d9909c74e6a11f917f758c
Silvio Ankermann <silvio@booq.org>
2024-10-02 18:25:15 +0200

    Revert "machines silvio-pc: use kernel 6.11.0-rc5"
    This reverts commit 88616488ef6534428bbf99a786674462c75ffb4b.

--- machines/silvio-pc/bcachefs.nix
+++ machines/silvio-pc/bcachefs.nix
@@ -1,11 +1,5 @@
 {
-  lib,
-  pkgs,
-  ...
-}: {
   boot.initrd.systemd.enable = false;
-
-  boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor pkgs.linux_6_11_rc5);
 
   fileSystems."/bcachefs" = {
     device = "UUID=677cf0a7-1abe-4ce3-876c-2ca63301229d";
