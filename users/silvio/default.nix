9cda6960220ef3d00e5ce989ac4ba574cdd46899
Silvio Ankermann <silvio@booq.org>
2024-12-21 14:13:08 +0100

    users silvio/sa: disable talon

--- users/silvio/default.nix
+++ users/silvio/default.nix
@@ -1,7 +1,7 @@
 {
   booq.gui.sway.enable = false;
   booq.gui.xmonad.enable = true;
-  booq.talon.enable = true;
+  booq.talon.enable = false;
   booq.vscode.useBlack = true;
 
   programs.git.userEmail = "silvio@booq.org";
