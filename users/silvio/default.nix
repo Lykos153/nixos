ab7b447dcfd4130c8131f6f2dcc9899e01d6342e
Silvio Ankermann <silvio@booq.org>
2024-12-31 02:13:30 +0100

    user silvio: enable jujutsu

--- users/silvio/default.nix
+++ users/silvio/default.nix
@@ -3,9 +3,12 @@
   booq.gui.xmonad.enable = true;
   booq.talon.enable = false;
   booq.vscode.useBlack = true;
+  booq.jujutsu.enable = true;
 
   programs.git.userEmail = "silvio@booq.org";
   programs.git.userName = "Silvio Ankermann";
+  programs.jujutsu.settings.user.name = "Silvio Ankermann";
+  programs.jujutsu.settings.user.email = "silvio@booq.org";
 
   imports = [
     ./mail.nix
