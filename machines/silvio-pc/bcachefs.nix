8f78e453a03e4a85a4c5cececc1156a13005fcc2
Silvio Ankermann <silvio@booq.org>
2024-12-31 02:43:29 +0100

    machines pc: use bcachefs master branch kernel

--- machines/silvio-pc/bcachefs.nix
+++ machines/silvio-pc/bcachefs.nix
@@ -1,5 +1,11 @@
 {
+  lib,
+  pkgs,
+  ...
+}: {
   boot.initrd.systemd.enable = false;
+
+  boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor pkgs.linux_bcachefs);
 
   fileSystems."/bcachefs" = {
     device = "UUID=677cf0a7-1abe-4ce3-876c-2ca63301229d";
