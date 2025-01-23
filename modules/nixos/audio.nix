b7df3309f547862d2090765864a845a5ea16cd0b
Silvio Ankermann <silvio@booq.org>
2025-01-23 22:50:38 +0100

    nixos audio: add pipewire bluetooth enhancements

--- modules/nixos/audio.nix
+++ modules/nixos/audio.nix
@@ -10,7 +10,7 @@ in {
     enable = lib.mkEnableOption "audio";
     backend = lib.mkOption {
       default = "pulseaudio";
-      type = lib.types.strMatching "pulseaudio|pipewire";
+      type = lib.types.enum ["pulseaudio" "pipewire"];
     };
   };
   config =
@@ -38,6 +38,14 @@ in {
         #     "default.clock.min-quantum" = 32;
         #     "default.clock.max-quantum" = 8192;
         # };
+      };
+      wireplumber.extraConfig.bluetoothEnhancements = {
+        "monitor.bluez.properties" = {
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-hw-volume" = true;
+          "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+        };
       };
       xdg.portal.wlr.enable = true;
     }
