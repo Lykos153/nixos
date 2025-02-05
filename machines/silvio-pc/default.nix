70fd4d22c05c3c9a08ca4b51ddf3d455f0db1619
Silvio Ankermann <silvio@booq.org>
2025-02-05 22:19:31 +0100

    machine pc: disable zram

--- machines/silvio-pc/default.nix
+++ machines/silvio-pc/default.nix
@@ -21,7 +21,7 @@
   hardware.logitech.wireless.enableGraphical = true;
 
   # TODO: luks+yubikey+secureboot https://www.reddit.com/r/NixOS/comments/xrgszw/comment/iqf1gps/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-  zramSwap.enable = true;
+  zramSwap.enable = false;
   # TODO evaluate https://github.com/vrmiguel/bustd instead
   services.earlyoom = {
     enable = true;
