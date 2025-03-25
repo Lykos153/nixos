5c0063f465667d6e81fe807eaf045f157ded7a84
Silvio Ankermann <silvio@booq.org>
2025-03-25 21:37:54 +0100

    machines silvio-pc: remove vfio settings

--- machines/silvio-pc/virtualisation.nix
+++ machines/silvio-pc/virtualisation.nix
@@ -1,14 +1,7 @@
-{pkgs, ...}: {
+{
   booq.virtualisation = {
     enable = true;
     libvirtUsers = ["silvio" "sa"];
   };
   virtualisation.spiceUSBRedirection.enable = true;
-
-  boot.kernelParams = ["intel_iommu=on" "vfio-pci.ids=10de:1380,10de:0fbc"];
-  boot.kernelModules = ["vfio-pci"];
-
-  # Blacklist nvidia because it's annoying.
-  # TODO: pcie passthrough using ie. https://github.com/CRTified/nur-packages
-  boot.blacklistedKernelModules = ["nvidia" "nouveau"];
 }
