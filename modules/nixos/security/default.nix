c95f7d5fffb947789a6df13f9de0b45f3040de76
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2025-07-16 11:22:20 +0200

    nixos security: use sudo-rs

--- modules/nixos/security/default.nix
+++ modules/nixos/security/default.nix
@@ -3,58 +3,9 @@
   config,
   pkgs,
   ...
-}: let
-  cfg = config.booq.securityKeys;
-in {
-  options.booq.securityKeys = {
-    enable = lib.mkEnableOption "securityKeys";
-    keyfile = lib.mkOption {
-      type = lib.types.path;
-    };
-  };
-  config = let
-    keyfile = "Yubico/u2f_keys";
-  in
-    lib.mkIf cfg.enable {
-      services.pcscd.enable = true;
-      hardware.gpgSmartcards.enable = true;
+}: {
+  imports = [./securityKeys.nix];
 
-      hardware.onlykey.enable = true;
-      environment.systemPackages = [
-        pkgs.onlykey
-        (pkgs.onlykey-cli.overrideAttrs (prev: {
-          version = "click";
-          src = pkgs.fetchFromGitHub {
-            owner = "lykos153";
-            repo = "python-onlykey";
-            rev = "refactor-cli";
-            sha256 = "sha256-5ErmqLHWA+aemirqWvi5/2ggOnscFUiCif2GF/qElSs=";
-          };
-        }))
-        pkgs.onlykey-agent
-      ];
-      services.upower.enable = true; # <- onlykey errors... but doesnt work anyway
-      services.udev.packages = [pkgs.yubikey-personalization];
-      security.pam = {
-        u2f = {
-          enable = false; # true would enable for all PAM, including ssh, see https://bytemeta.vip/repo/NixOS/nixpkgs/issues/166076
-
-          settings = {
-            # debug = true;
-            # control = "required"; # Require password AND Yubikey
-            cue = true; # show prompt
-            origin = "pam://nixos-silvio";
-            authFile = "/etc/${keyfile}";
-          };
-        };
-        # Only enable Yubikey for the following services
-        services.sudo.u2fAuth = true;
-        services.su.u2fAuth = true;
-        services.polkit-1.u2fAuth = true;
-        # services.sshd.u2fAuth = false;
-        # services.login.u2fAuth = false;
-        # services.swaylock.u2fAuth = false;
-      };
-      environment.etc."${keyfile}".source = cfg.keyfile;
-    };
+  security.sudo-rs.enable = true;
+  security.sudo-rs.wheelNeedsPassword = config.security.sudo.wheelNeedsPassword;
 }
