6f55a754620e1248d0d0422880c1a06a819f4a41
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2025-08-11 08:06:48 +0200

    hm gui autorandr: update screen position

--- modules/homeManager/gui/autorandr.nix
+++ modules/homeManager/gui/autorandr.nix
@@ -107,20 +107,20 @@ in
               enable = true;
               primary = true;
               mode = "3840x1080";
-              position = "0x0";
+              position = "1080x271";
               rate = "59.97";
             };
             laptop = {
               enable = true;
               mode = "1920x1080";
-              position = "960x1080";
+              position = "2040x1351";
               rate = "60.00";
             };
             benq = {
               enable = true;
               mode = "1920x1080";
-              position = "3840x0";
-              rotate = "left";
+              position = "0x0";
+              rotate = "right";
               rate = "60.00";
             };
           };
