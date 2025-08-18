f8deb8423eebb2780947037a3fdee0d7befed78e
Silvio Ankermann <silvio@booq.org>
2025-08-18 09:53:19 +0200

    WIP: dedicated waybar module

---
+++ modules/homeManager/gui/waybar/default.nix
@@ -0,0 +1,157 @@
+# Originally from https://git.sbruder.de/simon/nixos-config/raw/commit/540f89bff111c2ff10f6d809f61806082616f981/users/simon/modules/sway/waybar.nix
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  # TODO: Deduplicate watch and toggle functions
+  watchUserUnitState = unit: started: stopped:
+    pkgs.writeShellScript "watch-user-unit-${unit}-state" ''
+      ${pkgs.systemd}/bin/journalctl --user -u ${unit} -t systemd -o cat -f \
+          | ${pkgs.gnugrep}/bin/grep --line-buffered -Eo '^(Started|Stopped)' \
+          | ${pkgs.jq}/bin/jq --unbuffered -Rc 'if . == "Started" then ${builtins.toJSON started} else ${builtins.toJSON stopped} end'
+    '';
+
+  toggleUserUnitState = unit:
+    pkgs.writeShellScript "toggle-user-unit-${unit}-state" ''
+      if ${pkgs.systemd}/bin/systemctl --user show ${unit} | ${pkgs.gnugrep}/bin/grep -q ActiveState=active; then
+          ${pkgs.systemd}/bin/systemctl --user stop ${unit}
+      else
+          ${pkgs.systemd}/bin/systemctl --user start ${unit}
+      fi
+    '';
+  # nerd fonts are abusing arabic which breaks latin text
+  # context: https://github.com/Alexays/Waybar/issues/628
+  lrm = "&#8206;";
+
+  # for fine-grained control over spacing
+  thinsp = "&#8201;";
+in
+  lib.mkIf (config.booq.gui.enable && config.booq.gui.river.enable) {
+    booq.fonts.enable = true;
+    home.packages = [pkgs.nerd-fonts.iosevka];
+    programs.waybar = {
+      enable = true;
+      settings = [
+        {
+          layer = "top";
+          position = "top";
+          height = 24;
+
+          modules-center = [
+          ];
+          modules-left = [
+            "river/tags"
+            "wlr/taskbar"
+          ];
+          modules-right = [
+            "custom/redshift"
+            "idle_inhibitor"
+            "backlight"
+            "pulseaudio"
+            "network"
+            "memory"
+            "cpu"
+            "battery"
+            "tray"
+            "clock"
+          ];
+
+          "sway/workspaces" = {
+            disable-scroll = false;
+            disable-scroll-wraparound = true;
+            enable-bar-scroll = true;
+          };
+          "sway/mode" = {
+            format = "{}";
+          };
+
+          tray = {
+            spacing = 5;
+          };
+          "custom/redshift" = {
+            exec =
+              watchUserUnitState
+              "gammastep"
+              {class = "active";}
+              {class = "inactive";};
+            on-click = toggleUserUnitState "gammastep";
+            return-type = "json";
+            format = "";
+            tooltip = false;
+          };
+          idle_inhibitor = {
+            format = "{icon}";
+            format-icons = {
+              activated = " ";
+              deactivated = " ";
+            };
+          };
+          backlight = {
+            format = "{percent}% {icon}";
+            format-icons = [" " " " " " " " " " " " " "];
+            on-scroll-up = "${pkgs.brightnessctl}/bin/brightnessctl -q set +5%";
+            on-scroll-down = "${pkgs.brightnessctl}/bin/brightnessctl -n5 -q set 5%-";
+          };
+          pulseaudio = {
+            format = "{volume}% {icon} {format_source}";
+            format-bluetooth = "{volume}% {icon} {format_source}";
+            format-bluetooth-muted = "${lrm}ﱝ${lrm}  {icon} {format_source}";
+            format-muted = "${lrm}ﱝ${lrm}  {format_source}";
+            format-source = "{volume}% ${thinsp}";
+            format-source-muted = "{volume}% ${thinsp}";
+            format-icons = {
+              car = " ";
+              default = ["奄" "奔" "墳"];
+              hands-free = " ";
+              headphone = " ";
+              headset = " ";
+              phone = " ";
+              portable = " ";
+            };
+            on-click = "${pkgs.pulseaudio}/bin/pactl set-sink-mute 0 toggle";
+            on-click-right = "${pkgs.pavucontrol}/bin/pavucontrol";
+          };
+          network = {
+            format-wifi = "{essid} ({signalStrength}%) 直 ";
+            format-ethernet = "{ipaddr}/{cidr}  ";
+            format-linked = "{ifname} (No IP)  ";
+            format-disconnected = "Disconnected ⚠ ";
+            format-alt = "{ifname}: {ipaddr}/{cidr}";
+            tooltip = false;
+            on-click-right = "${config.programs.alacritty.package}/bin/alacritty -e ${pkgs.networkmanager}/bin/nmtui";
+          };
+          memory = {
+            interval = 2;
+            format = "{:2}%  ";
+          };
+          cpu = {
+            interval = 2;
+            format = "{usage:2}% ﬙ ";
+            tooltip = false;
+          };
+          battery = {
+            interval = 5;
+            format = "{capacity}% {icon}";
+            format-charging = "{capacity}% ";
+            format-plugged = "{capacity}% ${lrm}ﮣ";
+            format-alt = "{time} {icon}";
+            format-icons = ["" "" "" "" "" "" "" "" "" "" ""];
+            states = {
+              critical = 15;
+              good = 95;
+              warning = 30;
+            };
+          };
+          clock = {
+            interval = 1;
+            format = "{:%a %Y-%m-%d (%V) %H:%M:%S %Z}";
+            on-click = "";
+            tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+          };
+        }
+      ];
+      style = ./waybar.css;
+    };
+  }
