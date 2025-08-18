affa8a93203eac52f5dd0425b3f4aa87d389b769
Silvio Ankermann <silvio@booq.org>
2025-08-18 09:53:39 +0200

    WIP: combined kanshi/autorandr module

--- modules/homeManager/gui/kanshi.nix
+++ modules/homeManager/gui/kanshi.nix
@@ -3,7 +3,8 @@
   lib,
   ...
 }:
-lib.mkIf config.booq.gui.sway.enable {
+# TODO: depend on booq.gui.wayland
+lib.mkIf config.booq.gui.river.enable {
   services.kanshi = {
     enable = true;
     profiles = {
