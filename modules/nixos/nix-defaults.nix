88506d463ab26c137799ba89a459171ac4c86d70
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2025-09-17 13:32:36 +0200

    nixos nix-defaults: gc only if threshold is reached

--- modules/nixos/nix-defaults.nix
+++ modules/nixos/nix-defaults.nix
@@ -12,26 +12,40 @@ in {
       type = lib.types.bool;
       default = true;
     };
+    gc.percentFree = lib.mkOption {
+      type = lib.types.ints.between 0 100;
+      default = 5;
+    };
   };
 
-  config.nix = lib.mkIf cfg.enable {
-    package = pkgs.nixVersions.stable;
-    settings.auto-optimise-store = true;
+  config = lib.mkIf cfg.enable {
+    nix = {
+      package = pkgs.nixVersions.stable;
+      settings.auto-optimise-store = true;
 
-    gc = lib.mkIf cfg.auto-gc {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 90d";
+      gc = lib.mkIf cfg.auto-gc {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 90d";
+      };
+
+      extraOptions = ''
+        experimental-features = nix-command flakes
+        min-free = ${toString (1024 * 1024 * 1024)}
+        max-free = ${toString (5 * 1024 * 1024 * 1024)}
+      '';
+
+      daemonCPUSchedPolicy = "idle";
+      daemonIOSchedClass = "idle";
+      daemonIOSchedPriority = 7;
     };
 
-    extraOptions = ''
-      experimental-features = nix-command flakes
-      min-free = ${toString (1024 * 1024 * 1024)}
-      max-free = ${toString (5 * 1024 * 1024 * 1024)}
+    systemd.services.nix-gc.script = lib.mkForce ''
+      if [[ "$(LANG=C ${pkgs.coreutils}/bin/df /nix/store | ${pkgs.jc}/bin/jc --df | ${pkgs.jq}/bin/jq '.[0].use_percent')" -lt ${toString (100 - cfg.gc.percentFree)} ]]; then
+        echo "Skipping garbage collection. Threshold of ${toString cfg.gc.percentFree} not reached."
+        exit
+      fi
+      exec ${config.nix.package.out}/bin/nix-collect-garbage ${config.nix.gc.options}
     '';
-
-    daemonCPUSchedPolicy = "idle";
-    daemonIOSchedClass = "idle";
-    daemonIOSchedPriority = 7;
   };
 }
