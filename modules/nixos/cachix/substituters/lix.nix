a009b01b0a0da3116e3397edacf8e85b4291d928
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2025-09-21 08:52:42 +0200

    nixos: remove cachix

--- modules/nixos/cachix/substituters/lix.nix
+++
@@ -1,9 +0,0 @@
-{
-  nix.settings.substituters = [
-    "https://cache.lix.systems"
-  ];
-
-  nix.settings.trusted-public-keys = [
-    "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
-  ];
-}
