23fd7747f0909f747f63e47561362e6abdd92bcc
Silvio Ankermann <silvio@booq.org>
2025-11-10 01:32:39 +0100

    machine stick2: remove

--- machines/stick2/sops.nix
+++
@@ -1,8 +0,0 @@
-{config, ...}: {
-  booq.sops.enable = true;
-  # needs to be the actual path, because sops is loaded before impermanence
-  sops.age.sshKeyPaths = [];
-  sops.age.keyFile = "${config.booq.impermanence.persistRoot}/var/lib/sops-nix/age.key";
-  sops.age.generateKey = true;
-  sops.gnupg.sshKeyPaths = [];
-}
