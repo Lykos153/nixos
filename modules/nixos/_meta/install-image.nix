3eb8cb6461812c3a80377890ee0d850915a97415
Silvio Ankermann <silvio@booq.org>
2026-01-18 23:42:42 +0100

    fix install-image.iso

--- modules/nixos/_meta/install-image.nix
+++ modules/nixos/_meta/install-image.nix
@@ -17,11 +17,24 @@ in {
     booq.users.enable = true;
 
     services.openssh.enable = true;
+    security.sudo.enable = lib.mkForce true;
+    security.sudo-rs.enable = lib.mkForce false;
     security.sudo.wheelNeedsPassword = false;
 
     # Filesystems from https://github.com/NixOS/nixpkgs/blob/1a88164cc03ff2b7f3e79c042bee932bce7f036d/nixos/modules/profiles/base.nix#L52C2-L53C67
     # minus ZFS plus NTFS and bcachefs
-    boot.supportedFilesystems = lib.mkForce ["btrfs" "cifs" "f2fs" "jfs" "ntfs" "reiserfs" "vfat" "xfs" "ntfs" "bcachefs"];
+    boot.supportedFilesystems = lib.mkForce [
+      "btrfs"
+      "cifs"
+      "f2fs"
+      "jfs"
+      "ntfs"
+      "reiserfs"
+      "vfat"
+      "xfs"
+      "ntfs"
+      "bcachefs"
+    ];
 
     # lvm options
     boot.initrd.kernelModules = [
