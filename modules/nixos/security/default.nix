d3afc70820866ff71a603939647f75925f79d3ba
Silvio Ankermann <silvio@booq.org>
2026-01-19 23:34:20 +0100

    nixos security: prohibit password auth via ssh

--- modules/nixos/security/default.nix
+++ modules/nixos/security/default.nix
@@ -6,6 +6,9 @@
 }: {
   imports = [./securityKeys.nix];
 
+  services.openssh.settings.PasswordAuthentication = false;
+  services.openssh.settings.PermitRootLogin = "prohibit-password";
+
   security.sudo-rs.enable = true;
   security.sudo-rs.wheelNeedsPassword = config.security.sudo.wheelNeedsPassword;
 }
