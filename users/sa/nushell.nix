afd4a951410c55adf5fe4e9aaf494465c313cda7
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2026-04-09 21:08:54 +0200

    users sa: update yk8s stuff

--- users/sa/nushell.nix
+++ users/sa/nushell.nix
@@ -11,7 +11,14 @@
       }
 
       def --env "vault ldap-login" [] {
-        $env.VAULT_TOKEN = (vault login -method=ldap -token-only username=silvio.ankermann)
+        let cacert_args = if ("VAULT_CACERT" in $env) { [--cacert $env.VAULT_CACERT] } else { [] }
+        $env.VAULT_TOKEN = (
+          {password: (pass ldap)}
+            | to json
+            | curl ...$cacert_args -X POST $"($env.VAULT_ADDR)/v1/auth/ldap/login/silvio.ankermann" -H "Content-Type: application/json" --silent --data @-
+            | from json
+            | get auth.client_token
+        )
       }
     '';
   };
