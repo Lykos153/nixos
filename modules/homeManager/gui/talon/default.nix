cec224f39ac64297d792a5ba57105c291ca8d6a7
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2026-05-04 17:04:27 +0200

    hm talon: move cursorless config to talon module

--- modules/homeManager/gui/talon/default.nix
+++ modules/homeManager/gui/talon/default.nix
@@ -35,6 +35,14 @@ in {
       open-vsx.pokey.parse-tree
       open-vsx.pokey.command-server
     ];
+    programs.vscode.userSettings."cursorless.colors.dark" = {
+      "default" = "#AAA7BB";
+      "blue" = "#0870FF";
+      "green" = "#36CC3F";
+      "red" = "#992222";
+      "pink" = "#FE7F9C";
+      "yellow" = "#E5C02C";
+    };
     home.file.".talon/user/cursorless-talon".source = pkgs.repos.cursorless-talon;
     # home.file.".talon/user/talon-community".source = pkgs.repos.talon-community;
     # TODO: define general method for dynamic config files. or move back to ^ once my config stabilizes
